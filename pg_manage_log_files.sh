b624cfa89f79db49a14bb8e7ffc39c7c2208ccdb
mpearson-py <98156698+mpearson-py@users.noreply.github.com>
2024-02-06 14:23:15 +0000

    Update pg_manage_log_files.sh

--- pg_manage_log_files.sh
+++ pg_manage_log_files.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-##set -x
+#set -x
 ##-------------------------------------------------------------------------------------------------------------##
 ##   Name:        pg_manage_log_files.sh
 ##   Purpose:     Script to manage PostgreSQL log files
@@ -20,6 +20,9 @@ export SCRIPT_NAME=$( basename $0 )
 export SCRIPT_PREFIX=$( echo "${SCRIPT_NAME}" | awk -F. '{ print $1 }' )
 export LOG_FILENAME=${SCRIPT_PREFIX}.log
 export HOSTNAME=$( hostname -s )
+export ERR_FILE=${SCRIPT_PREFIX}.err
+
+cat /dev/null > ${ERR_FILE}
 
 ## Global variables
 
@@ -116,16 +119,61 @@ fi
 ## list files to delete
 
 echo -e "Script ${SCRIPT_NAME} started at: $( date )\n" | tee -a $LOG_FILENAME
-echo -e "List all files to be removed from $LOG_DIR\n" | tee -a $LOG_FILENAME
+echo -e "Diskspace size before maintenance:\n$( df -hP $LOG_DIR )\n" | tee -a $LOG_FILENAME
 
-find $LOG_DIR -name "$LOG_FORMAT" -mtime +${REMOVE_DATE} -exec ls -l {} \; | tee -a $LOG_FILENAME
+REMOVE_FILES_LIST=$( find $LOG_DIR -name "$LOG_FORMAT" -mtime +${REMOVE_DATE} 2>$ERR_FILE )
 
-[ $VERBOSE ] && echo -e "Command to find and delete oldest files:  find $LOG_DIR -name \"$LOG_FORMAT\" -mtime +${REMOVE_DATE} -exec ls -l {} \;"
+if [[ $? -ne 0 || -s $ERR_FILE ]]; then
+    echo -e "Error running the find command in the $LOG_DIR for the delete statement.\n$( cat $ERR_FILE )"
+    exit
+fi
+
+[ $VERBOSE ] && echo -e "Cmd to find oldest files to delete: find $LOG_DIR -name \"$LOG_FORMAT\" -mtime +${REMOVE_DATE}"
+
+## Loop though the list and remove the files
+
+for FILE in ${REMOVE_FILES_LIST}
+do
+    [ $VERBOSE ] && echo -e "Removing file: $FILE from $LOG_DIR"
+
+    rm -f ${FILE}
+
+    if [ $? -ne 0 ]; then
+        echo -e "File: $FILE has not been successfully removed from directory: $LOG_DIR" | tee -a $LOG_FILENAME
+    else
+        echo -e "File: $FILE has been successfully removed from directory: $LOG_DIR" | tee -a $LOG_FILENAME
+    fi
+
+done
 
 ## List files to be compressed
 
 echo -e "List all files to be compressed over $FILE_AGE days old from $LOG_DIR\n" | tee -a $LOG_FILENAME
 
-find $LOG_DIR -name "$LOG_FORMAT" -mtime +${FILE_AGE} -exec ls -l {} \; | tee -a $LOG_FILENAME
+COMPRESS_FILE_LIST=$( find $LOG_DIR -name "$LOG_FORMAT" -mtime +${FILE_AGE} 2>$ERR_FILE )
 
-echo -e "Script: ${SCRIPT_NAME} completed at: $( date )\n" | tee -a $LOG_FILENAME
+if [[ $? -ne 0 || -s $ERR_FILE ]]; then
+    echo -e "Error running the find command in the $LOG_DIR for the compression routine.\n$( cat $ERR_FILE )"
+    exit
+fi
+
+[ $VERBOSE ] && echo -e "Cmd to find oldest files to compress: find $LOG_DIR -name \"$LOG_FORMAT\" -mtime +${FILE_AGE}"
+
+## Loop though the list and remove the files
+
+for FILE in $( echo -e ${COMPRESS_FILE_LIST} | grep -v gz$ )
+do
+    [ $VERBOSE ] && echo -e "Removing file: $FILE from $LOG_DIR"
+
+    gzip ${FILE}
+
+    if [ $? -ne 0 ]; then
+        echo -e "File: $FILE has not been successfully compressed in directory: $LOG_DIR" | tee -a $LOG_FILENAME
+    else
+        echo -e "File: $FILE has been successfully compressed in directory: $LOG_DIR" | tee -a $LOG_FILENAME
+    fi
+
+done
+
+echo -e "Diskspace size after logs maintenance:\n$( df -hP $LOG_DIR )\n" | tee -a $LOG_FILENAME
+echo -e "Script: ${SCRIPT_NAME} completed at: $( date )\n"               | tee -a $LOG_FILENAME
